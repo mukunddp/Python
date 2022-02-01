39aceb381f2202586d9a8622a71383c0e759d76c
Mukund Parve <98021056+mukunddp@users.noreply.github.com>
2022-02-01 15:31:08 +0530

    Delete student.py

--- student.py
+++
@@ -1,9 +0,0 @@
-
-class Student:
-    def mukund(self):
-        self.name = input("Enter name : ")
-        self.age = input("Enter age: ")
-
-o = Student()
-o.mukund()
-print("My Name is", o.name, "and my roll number is", o.age)
